62a8e34dc189ca2ead2fd6e98d4d118407488301
g18d5811 <g18d5811@HUG126>
2020-02-22 11:27:18 +0200

    first commit

--- Problems.Tests/UnitTest1.cs
+++ Problems.Tests/UnitTest1.cs
@@ -1,18 +1,41 @@
 using NUnit.Framework;
+using Problems;
 
 namespace Problems.Tests
 {
-    public class Tests
+     public class Tests
     {
         [SetUp]
         public void Setup()
         {
+
+            
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            
+            
+            string u = Program.Prefix("hello");
+            Assert.AreEqual("5,1:hello",u);
+            
+        }
+
+        [Test]
+        public void Test2()
+        {
+            // Program p = new Program();
+            string u = Program.Prefix("");
+            Assert.AreEqual("0,0:",u);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            // Program p = new Program();
+            string u = Program.Prefix("what   ...   did you say?? ");
+            Assert.AreEqual("27,5:what   ...   did you say?? ",u);
         }
     }
 }
