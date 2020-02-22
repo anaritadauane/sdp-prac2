369177495cdc2679a268db2c69ae12a46c83144d
g18d5811 <g18d5811@HUG126>
2020-02-22 11:28:07 +0200

    Program commit

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,38 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string word = "hello";
+            Prefix(word);
+        }
+
+        public static string Prefix(string input)
+        {
+             
+            int str = input.Length;
+            string[] words = input.Split(" ");
+            int counter = 0;
+            int num_Words = words.Length;
+
+            if (input == ""){
+                num_Words = 0;
+            }
+
+            foreach(string i in words){
+                if (i != "")
+                {
+                    counter++;
+                }
+            }
+
+            //Console.WriteLine( str + "," + num_Words + ":" + input);
+            string answer = str + "," + counter + ":" + input;
+            return answer;
+            
         }
     }
 }
